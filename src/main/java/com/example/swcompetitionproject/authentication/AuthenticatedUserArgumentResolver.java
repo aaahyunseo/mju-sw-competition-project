d1db40c18dca2013ee8cf1eb76fea6b9ff489568
aaahyunseo <hs030608@naver.com>
2024-08-12 20:13:21 +0900

    feat: AuthenticatedUserArgumentResolver 생성

---
+++ src/main/java/com/example/swcompetitionproject/authentication/AuthenticatedUserArgumentResolver.java
@@ -0,0 +1,28 @@
+package com.example.swcompetitionproject.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticatedUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final AuthenticationContext authenticationContext;
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticatedUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter,
+                                  final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest,
+                                  final WebDataBinderFactory binderFactory) throws Exception {
+        return authenticationContext.getPrincipal();
+    }
+}
