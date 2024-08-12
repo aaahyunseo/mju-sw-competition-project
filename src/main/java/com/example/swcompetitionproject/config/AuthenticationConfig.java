dbb0577c95d2751c84b138ac3441e3e253baa0cc
aaahyunseo <hs030608@naver.com>
2024-08-12 20:23:37 +0900

    feat: AuthenticationConfig 설정

---
+++ src/main/java/com/example/swcompetitionproject/config/AuthenticationConfig.java
@@ -0,0 +1,30 @@
+package com.example.swcompetitionproject.config;
+
+import com.example.swcompetitionproject.authentication.AuthenticatedUserArgumentResolver;
+import com.example.swcompetitionproject.authentication.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig implements WebMvcConfigurer {
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final AuthenticatedUserArgumentResolver authenticatedUserArgumentResolver;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/planners/**", "/diaries/**", "/rooms/**", "/users/**", "/surveys", "/auth/logout")
+                .excludePathPatterns("/auth/signup", "/auth/login", "/auth/refresh");
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedUserArgumentResolver);
+    }
+}
