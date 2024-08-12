914d3072158bf3a02f5624a515d30faa6bccfc9e
aaahyunseo <hs030608@naver.com>
2024-08-12 20:15:42 +0900

    feat: UnauthorizedException 디테일 추가

--- src/main/java/com/example/swcompetitionproject/exception/UnauthorizedException.java
+++ src/main/java/com/example/swcompetitionproject/exception/UnauthorizedException.java
@@ -1,7 +1,8 @@
 package com.example.swcompetitionproject.exception;
 
 public class UnauthorizedException extends CustomException {
-    public UnauthorizedException(ErrorCode errorCode) {
-        super(errorCode);
+    public UnauthorizedException(ErrorCode errorCode, String detail) {
+        super(errorCode, detail);
     }
+    public UnauthorizedException(ErrorCode errorCode) {super(errorCode);}
 }
