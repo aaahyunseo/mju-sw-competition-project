59bf135f7f18113d3604c8b932e141dcf2395466
aaahyunseo <hs030608@naver.com>
2024-08-12 18:43:57 +0900

    feat: BaseEntity 생성

---
+++ src/main/java/com/example/swcompetitionproject/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.swcompetitionproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid2")
+    @Column(updatable = false, unique = true, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+}
