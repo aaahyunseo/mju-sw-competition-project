230e65521f9758a6c20c6f8be5fcce04f9c3d9d8
juryheed <rabitangl20@naver.com>
2024-08-12 20:02:23 +0900

    feat: NotFoundException 작성 - #5

---
+++ src/main/java/com/example/swcompetitionproject/exception/NotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.swcompetitionproject.exception;
+public class NotFoundException extends CustomException {
+    public NotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
