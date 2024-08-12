8da9a7deea1df59cff88085bbc874ce042b9abc5
aaahyunseo <hs030608@naver.com>
2024-08-12 12:47:49 +0900

    feat: spring initializing - #1

---
+++ src/main/java/com/example/swcompetitionproject/SwCompetitionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.swcompetitionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwCompetitionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwCompetitionProjectApplication.class, args);
+    }
+
+}
