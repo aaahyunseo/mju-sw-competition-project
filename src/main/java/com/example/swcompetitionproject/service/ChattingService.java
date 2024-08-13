690e17502ae24365aa1901f2356a93f51809052f
aaahyunseo <hs030608@naver.com>
2024-08-14 05:27:58 +0900

    feat: ChattingService 추가 - #15

---
+++ src/main/java/com/example/swcompetitionproject/service/ChattingService.java
@@ -0,0 +1,100 @@
+package com.example.swcompetitionproject.service;
+
+import com.example.swcompetitionproject.dto.request.chatting.ChatRequestDto;
+import com.example.swcompetitionproject.dto.request.chatting.CreateRoomDto;
+import com.example.swcompetitionproject.dto.response.chatting.ChattingRoomListData;
+import com.example.swcompetitionproject.entity.*;
+import com.example.swcompetitionproject.exception.ErrorCode;
+import com.example.swcompetitionproject.exception.NotFoundException;
+import com.example.swcompetitionproject.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ChattingService {
+
+    private final ChattingRoomRepository chattingRoomRepository;
+    private final MessageRepository messageRepository;
+    private final RoomMemberRepository roomMemberRepository;
+    private final UserRoomRepository userRoomRepository;
+
+    //채팅방 목록 조회
+    @Transactional
+    public ChattingRoomListData getRoomList(User user) {
+        List<ChattingRoom> rooms = userRoomRepository.findByUser(user).stream()
+                .map(UserRoom::getChattingRoom)
+                .collect(Collectors.toList());
+
+        return ChattingRoomListData.from(rooms);
+    }
+
+    //채팅방 생성하기 - 게시글 생성 시 채팅방 생성
+    @Transactional
+    public ChattingRoom createRoom(User user, CreateRoomDto createRoomDto) {
+        ChattingRoom room = ChattingRoom.builder()
+                .title(createRoomDto.getTitle())
+                .manager(user.getName())
+                .memberCount(1)
+                .build();
+
+        UserRoom userRoom = UserRoom.builder()
+                .user(user)
+                .chattingRoom(room)
+                .build();
+        userRoomRepository.save(userRoom);
+
+        return chattingRoomRepository.save(room);
+    }
+
+    //채팅 메시지 저장
+    @Transactional
+    public Message saveMessage(User user, ChatRequestDto message) {
+        ChattingRoom room = chattingRoomRepository.findById(message.getRoomId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.ROOM_NOT_FOUND));
+
+        Message newMessage = Message.builder()
+                .content(message.getContent())
+                .sender(user.getName())
+                .chattingRoom(room)
+                .build();
+
+        return messageRepository.save(newMessage);
+    }
+
+    //사용자를 채팅방에 추가
+    @Transactional
+    public void addUserToRoom(User user, ChatRequestDto message) {
+        ChattingRoom room = chattingRoomRepository.findById(message.getRoomId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.ROOM_NOT_FOUND));
+
+        RoomMember member = RoomMember.builder()
+                .name(user.getName())
+                .chattingRoom(room)
+                .build();
+        roomMemberRepository.save(member);
+
+        room.setMemberCount(room.getMemberCount() + 1);
+        chattingRoomRepository.save(room);
+    }
+
+    // 채팅방 퇴장하기
+    @Transactional
+    public void removeUserFromRoom(User user, ChatRequestDto message) {
+        ChattingRoom room = chattingRoomRepository.findById(message.getRoomId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.ROOM_NOT_FOUND));
+
+        RoomMember member = roomMemberRepository.findByNameAndChattingRoom(user.getName(), room)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        roomMemberRepository.delete(member);
+
+        room.setMemberCount(room.getMemberCount() - 1);
+        chattingRoomRepository.save(room);
+    }
+}
+
