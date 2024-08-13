1ad171c32962a8e6419337165474816db90b466f
aaahyunseo <hs030608@naver.com>
2024-08-14 05:31:35 +0900

    feat: RoomMemberRepository 메서드 추가 - #15

--- src/main/java/com/example/swcompetitionproject/repository/RoomMemberRepository.java
+++ src/main/java/com/example/swcompetitionproject/repository/RoomMemberRepository.java
@@ -1,9 +1,12 @@
 package com.example.swcompetitionproject.repository;
 
+import com.example.swcompetitionproject.entity.ChattingRoom;
 import com.example.swcompetitionproject.entity.RoomMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoomMemberRepository extends JpaRepository<RoomMember, UUID> {
+    Optional<RoomMember> findByNameAndChattingRoom(String name, ChattingRoom chattingRoom);
 }
