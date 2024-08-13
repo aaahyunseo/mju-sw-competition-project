39b1fac08327367ac14e18ff45f34e40cf4c43bd
aaahyunseo <hs030608@naver.com>
2024-08-14 05:33:32 +0900

    feat: UserRepository 메서드 추가 - #15

--- src/main/java/com/example/swcompetitionproject/repository/UserRepository.java
+++ src/main/java/com/example/swcompetitionproject/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.swcompetitionproject.repository;
 import com.example.swcompetitionproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByStudentNumber(String number);
 }
