d55bc3a176e34defc2ec3995c8bd5f50f3b044cc
aaahyunseo <hs030608@naver.com>
2024-08-14 05:27:43 +0900

    feat: ChattingRoomListDto 추가 - #15

---
+++ src/main/java/com/example/swcompetitionproject/dto/response/chatting/ChattingRoomListDto.java
@@ -0,0 +1,23 @@
+package com.example.swcompetitionproject.dto.response.chatting;
+
+import com.example.swcompetitionproject.entity.ChattingRoom;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+public class ChattingRoomListDto {
+    private UUID id;
+    private String title;
+    private int memberCount;
+
+    public static ChattingRoomListDto from(ChattingRoom room){
+        return ChattingRoomListDto.builder()
+                .id(room.getId())
+                .title(room.getTitle())
+                .memberCount(room.getMemberCount())
+                .build();
+    }
+}
