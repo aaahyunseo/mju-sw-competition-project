3755831ed87b5c861b30e6c02d600b91d3b6fcf1
juryheed <rabitangl20@naver.com>
2024-08-14 19:45:34 +0900

    feat: 로그인 Dto - #16 id, passwrd있음

---
+++ src/main/java/com/example/swcompetitionproject/dto/response/request/LoginDto.java
@@ -0,0 +1,17 @@
+package com.example.swcompetitionproject.dto.response.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginDto {
+
+    @NotBlank(message = "아이디를 입력해 주세요")
+    private String id;
+
+    @NotBlank(message = "비밀번호를 입력해 주세요")
+    private String passwrd;
+}
