0536a8a575e5666d66e7f0c2486121bcb3b057f0
aaahyunseo <hs030608@naver.com>
2024-08-15 03:24:46 +0900

    feat: ChattingRoomController 채팅방 생성 로직 삭제 - #15

--- src/main/java/com/example/swcompetitionproject/controller/ChattingRoomController.java
+++ src/main/java/com/example/swcompetitionproject/controller/ChattingRoomController.java
@@ -1,7 +1,6 @@
 package com.example.swcompetitionproject.controller;
 
 import com.example.swcompetitionproject.authentication.AuthenticatedUser;
-import com.example.swcompetitionproject.dto.request.chatting.CreateRoomDto;
 import com.example.swcompetitionproject.dto.response.ResponseDto;
 import com.example.swcompetitionproject.dto.response.chatting.ChattingRoomListData;
 import com.example.swcompetitionproject.entity.User;
@@ -22,12 +21,5 @@ public class ChattingRoomController {
     public ResponseEntity<ResponseDto<ChattingRoomListData>> getRoomList(@AuthenticatedUser User user) {
         ChattingRoomListData chattingRoomListData = chattingService.getRoomList(user);
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "채팅 목록 조회 완료", chattingRoomListData), HttpStatus.OK);
-    }
-
-    // 채팅방 생성
-    @PostMapping
-    public ResponseEntity<ResponseDto<Void>> createRoom(@AuthenticatedUser User user, @RequestBody CreateRoomDto createRoomDto) {
-        chattingService.createRoom(user, createRoomDto);
-        return new ResponseEntity<>(ResponseDto.res(HttpStatus.CREATED, "채팅방 생성 완료"), HttpStatus.CREATED);
     }
 }
