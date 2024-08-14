9b7e53a64f8a04d564130b9f9d83cd9c0995915b
juryheed <rabitangl20@naver.com>
2024-08-15 07:12:28 +0900

    fix: 로그인 Dto파이루이치 변경 - #16

--- src/main/java/com/example/swcompetitionproject/dto/request/LoginDto.java
+++ src/main/java/com/example/swcompetitionproject/dto/request/LoginDto.java
@@ -1,4 +1,4 @@
-package com.example.swcompetitionproject.dto.response.request;
+package com.example.swcompetitionproject.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
