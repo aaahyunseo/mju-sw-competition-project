57f8f506c76e39da83294991049612a1d9532705
juryheed <rabitangl20@naver.com>
2024-08-15 20:19:35 +0900

    feat: MypageController작성 - #22 유저이름 변경하는 modifyUserName메서드 경로는 users/info/name

---
+++ src/main/java/com/example/swcompetitionproject/controller/MypageController.java
@@ -0,0 +1,30 @@
+package com.example.swcompetitionproject.controller;
+
+import com.example.swcompetitionproject.authentication.AuthenticatedUser;
+import com.example.swcompetitionproject.dto.request.ModifyUserNameDto;
+import com.example.swcompetitionproject.dto.response.ResponseDto;
+import com.example.swcompetitionproject.entity.User;
+import com.example.swcompetitionproject.service.MyPageService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class MypageController {
+    private final MyPageService myPageService;
+
+    /**
+     * 유저 이름 변경
+     */
+    @PatchMapping("info/name")
+    public ResponseEntity<ResponseDto<Void>> modifyUserName(@AuthenticatedUser User user, @RequestBody ModifyUserNameDto modifyUserNameDto){
+        myPageService.modifyUserName(user,modifyUserNameDto);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "이름 수정 완료"), HttpStatus.OK);
+    }
+}
