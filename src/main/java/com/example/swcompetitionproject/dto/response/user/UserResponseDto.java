6cbab1fc1ff8641f0382584c2533918b6c85acbd
juryheed <rabitangl20@naver.com>
2024-08-15 23:54:02 +0900

    feat: UserResponseDto등록 - #24 - 유저 정보를 받아오는 역할

---
+++ src/main/java/com/example/swcompetitionproject/dto/response/user/UserResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.swcompetitionproject.dto.response.user;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponseDto {
+    //이름
+    private String name;
+
+    //전공(소속)
+    private String major;
+
+    //학번
+    private String studentNumber;
+
+}
