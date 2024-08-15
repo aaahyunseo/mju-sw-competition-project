fddfa4184a7fe1c9a7b2088a0d193df1bfff72cc
juryheed <rabitangl20@naver.com>
2024-08-16 01:46:07 +0900

    feat: 나의 게시판을 조회하는 비즈니스 로직 - #34 - 유저로 내가 작성한 게시판을 모두 찾는다. - MyBoardListData로 반환 한다.

--- src/main/java/com/example/swcompetitionproject/service/MyPageService.java
+++ src/main/java/com/example/swcompetitionproject/service/MyPageService.java
@@ -1,17 +1,23 @@
 package com.example.swcompetitionproject.service;
 
 import com.example.swcompetitionproject.dto.request.user.ModifyUserNameDto;
+import com.example.swcompetitionproject.dto.response.board.MyBoardListData;
 import com.example.swcompetitionproject.dto.response.user.UserResponseDto;
+import com.example.swcompetitionproject.entity.Board;
 import com.example.swcompetitionproject.entity.User;
+import com.example.swcompetitionproject.repository.BoardRepository;
 import com.example.swcompetitionproject.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class MyPageService {
 
     private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
 
     /**
      * 유저 이름 변경
@@ -32,6 +38,19 @@ public class MyPageService {
                 .studentNumber(user.getStudentNumber())
                 .build();
         return userResponseDto;
+    }
+
+    /**
+     * 나의 게시판 조회
+     */
+    public MyBoardListData getUserBoard(User user){
+        //내가 작성한 모든 게시판 찾기
+        List<Board> myBoards=boardRepository.findAllByUser(user);
+
+        //데이터 리스트로 만들기
+        MyBoardListData myBoardListData = MyBoardListData.from(myBoards);
+
+        return myBoardListData;
     }
 
 }
