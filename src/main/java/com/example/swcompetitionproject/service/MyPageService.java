0b15973c8a25840f34b220b0a9cebbb2a7dfeec4
juryheed <rabitangl20@naver.com>
2024-08-15 23:56:29 +0900

    feat: 유저 정보조회 서비스 작성 - #24 - userResponseDto에 정보를 수집하는 로직

--- src/main/java/com/example/swcompetitionproject/service/MyPageService.java
+++ src/main/java/com/example/swcompetitionproject/service/MyPageService.java
@@ -1,6 +1,7 @@
 package com.example.swcompetitionproject.service;
 
 import com.example.swcompetitionproject.dto.request.user.ModifyUserNameDto;
+import com.example.swcompetitionproject.dto.response.user.UserResponseDto;
 import com.example.swcompetitionproject.entity.User;
 import com.example.swcompetitionproject.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -15,9 +16,22 @@ public class MyPageService {
     /**
      * 유저 이름 변경
      */
-    public void modifyUserName(User user,ModifyUserNameDto modifyUserNameDto){
+    public void modifyUserName(User user, ModifyUserNameDto modifyUserNameDto) {
         //유저의 이름만 변경
         user.setName(modifyUserNameDto.getName());
         userRepository.save(user);
     }
+
+    /**
+     * 유저 정보 조회
+     */
+    public UserResponseDto getUserInfo(User user) {
+        UserResponseDto userResponseDto = UserResponseDto.builder()
+                .name(user.getName())
+                .major(user.getMajor())
+                .studentNumber(user.getStudentNumber())
+                .build();
+        return userResponseDto;
+    }
+
 }
