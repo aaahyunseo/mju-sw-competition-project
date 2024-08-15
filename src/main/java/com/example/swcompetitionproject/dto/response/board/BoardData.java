b916078fe32a40873da76347f79242b7509fad8e
aaahyunseo <hs030608@naver.com>
2024-08-16 08:27:34 +0900

    feat: BoardData 카테고리 리스트 dto 추가 - #41

--- src/main/java/com/example/swcompetitionproject/dto/response/board/BoardData.java
+++ src/main/java/com/example/swcompetitionproject/dto/response/board/BoardData.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.UUID;
 
 @Builder
@@ -13,6 +14,7 @@ public class BoardData {
     private UUID id;
     private String title;
     private String content;
+    private List<String> categoryList;
     private int total;
     private String createdAt;
 
@@ -21,6 +23,7 @@ public class BoardData {
                 .id(board.getId())
                 .title(board.getTitle())
                 .content(board.getContent())
+                .categoryList(board.getUser().getCategory())
                 .total(board.getTotal())
                 .createdAt(board.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
                 .build();
