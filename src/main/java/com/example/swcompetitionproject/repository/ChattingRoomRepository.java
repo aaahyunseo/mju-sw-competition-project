ba63a062b0a9259bd1fb1bd5f74422d6fa1499b0
aaahyunseo <hs030608@naver.com>
2024-08-15 21:19:52 +0900

    feat: ChattingRoomRepository 메서드 추가 - #15

--- src/main/java/com/example/swcompetitionproject/repository/ChattingRoomRepository.java
+++ src/main/java/com/example/swcompetitionproject/repository/ChattingRoomRepository.java
@@ -1,9 +1,12 @@
 package com.example.swcompetitionproject.repository;
 
+import com.example.swcompetitionproject.entity.Board;
 import com.example.swcompetitionproject.entity.ChattingRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, UUID> {
+    Optional<ChattingRoom> deleteByBoard(Board board);
 }
