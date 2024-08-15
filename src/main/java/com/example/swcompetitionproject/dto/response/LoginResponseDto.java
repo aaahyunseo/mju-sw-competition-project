28a629c875d25084507c667b9db16b7c907152c8
juryheed <rabitangl20@naver.com>
2024-08-15 22:32:38 +0900

    feat: LoginResponseDto등록 - #28 - 로그인 시에만 사용하는 LoginResponseDto작성 - statusCode,message,accessToken을 포함

---
+++ src/main/java/com/example/swcompetitionproject/dto/response/LoginResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.swcompetitionproject.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto<T> {
+    private final String statusCode;
+    private final String message;
+    private final String accessToken;
+
+    //로그인시 사용하는 응답
+    // 반환 데이터가 있는 API를 위한 응답
+    public static <T> LoginResponseDto<T> loginres(final HttpStatusCode statusCode, final String message,final String accessToken) {
+        return new LoginResponseDto<>(statusCode.toString(), message,accessToken);
+    }
+}
