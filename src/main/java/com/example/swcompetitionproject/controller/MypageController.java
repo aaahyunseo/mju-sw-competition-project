7021be76c1b0e3e3b8c76dbaf741f803723eef0f
juryheed <rabitangl20@naver.com>
2024-08-15 20:49:18 +0900

    fix: name을 입력하지 않아도 수정이 되는 버그 수정 - #22 modifyUserName컨트롤러에 @Valid를 설정함 Dto에서 오류를 거르지 못하는 문제를 해결

--- src/main/java/com/example/swcompetitionproject/controller/MypageController.java
+++ src/main/java/com/example/swcompetitionproject/controller/MypageController.java
@@ -5,6 +5,7 @@ import com.example.swcompetitionproject.dto.request.ModifyUserNameDto;
 import com.example.swcompetitionproject.dto.response.ResponseDto;
 import com.example.swcompetitionproject.entity.User;
 import com.example.swcompetitionproject.service.MyPageService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class MypageController {
      * 유저 이름 변경
      */
     @PatchMapping("info/name")
-    public ResponseEntity<ResponseDto<Void>> modifyUserName(@AuthenticatedUser User user, @RequestBody ModifyUserNameDto modifyUserNameDto){
+    public ResponseEntity<ResponseDto<Void>> modifyUserName(@AuthenticatedUser User user, @Valid @RequestBody ModifyUserNameDto modifyUserNameDto){
         myPageService.modifyUserName(user,modifyUserNameDto);
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "이름 수정 완료"), HttpStatus.OK);
     }
