e4b89250667fc7a33fa7b0e94658c7e14f4f6e0a
aaahyunseo <hs030608@naver.com>
2024-08-15 21:21:07 +0900

    feat: CreateBoardDto 추가 - #18

---
+++ src/main/java/com/example/swcompetitionproject/dto/request/board/CreateBoardDto.java
@@ -0,0 +1,20 @@
+package com.example.swcompetitionproject.dto.request.board;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateBoardDto {
+    @NotBlank(message = "게시글 제목을 입력하세요.")
+    @Size(max = 20)
+    private String title;
+    @NotBlank(message = "게시글 내용을 입력하세요.")
+    @Size(max = 500)
+    private String content;
+}
+
