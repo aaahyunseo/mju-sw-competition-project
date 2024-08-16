5286203cf2d8045f6d7bf1ccb0513d38b8aa1bdb
aaahyunseo <hs030608@naver.com>
2024-08-16 20:15:45 +0900

    feat: BoardDetailResponseDto 카테고리 조회 추가 - #48

--- src/main/java/com/example/swcompetitionproject/dto/response/board/BoardDetailResponseDto.java
+++ src/main/java/com/example/swcompetitionproject/dto/response/board/BoardDetailResponseDto.java
@@ -1,12 +1,15 @@
 package com.example.swcompetitionproject.dto.response.board;
 
 import com.example.swcompetitionproject.entity.Board;
+import com.example.swcompetitionproject.entity.BoardCategory;
+import com.example.swcompetitionproject.entity.Category;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
@@ -14,7 +17,7 @@ public class BoardDetailResponseDto {
     private UUID id;
     private String title;
     private String content;
-    //private List<String> categoryList;
+    private List<String> categoryList;
     private int current;
     private int total;
     private String createdAt;
@@ -24,7 +27,9 @@ public class BoardDetailResponseDto {
                 .id(board.getId())
                 .title(board.getTitle())
                 .content(board.getContent())
-                //.categoryList(board.getUser().getCategory())
+                .categoryList(board.getBoardCategories().stream()
+                        .map(boardCategory -> boardCategory.getCategory().getCategory())
+                        .collect(Collectors.toList()))
                 .current(board.getChattingRoom().getMemberCount())
                 .total(board.getTotal())
                 .createdAt(board.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
