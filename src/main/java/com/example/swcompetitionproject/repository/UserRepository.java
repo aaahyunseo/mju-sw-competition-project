6bfcfdbc0272d8f15939997c4fa8b1bed39a17b7
aaahyunseo <hs030608@naver.com>
2024-08-16 20:18:48 +0900

    feat: UserRepository 유저 불러올 때 카테고리 로딩 설정 추가 - #48

--- src/main/java/com/example/swcompetitionproject/repository/UserRepository.java
+++ src/main/java/com/example/swcompetitionproject/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.swcompetitionproject.repository;
 
 import com.example.swcompetitionproject.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,7 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByStudentNumber(String studentNumber);
+
+    @EntityGraph(attributePaths = "category")
+    Optional<User> findById(UUID id);
 }
