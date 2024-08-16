0c961d91b6dc52b2bb88d301d0ef25ea5968231e
aaahyunseo <hs030608@naver.com>
2024-08-16 20:13:28 +0900

    feat: BoardCategory 엔티티 생성 - #48

---
+++ src/main/java/com/example/swcompetitionproject/entity/BoardCategory.java
@@ -0,0 +1,24 @@
+package com.example.swcompetitionproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "board_category")
+public class BoardCategory extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+}
