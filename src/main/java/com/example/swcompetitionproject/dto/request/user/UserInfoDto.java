0ee5921b4d89a42fa368f72662fc02435efc6ad8
aaahyunseo <hs030608@naver.com>
2024-08-17 23:03:57 +0900

    feat: UserInfoDto 성별 정보 필드 추가 - #58

--- src/main/java/com/example/swcompetitionproject/dto/request/user/UserInfoDto.java
+++ src/main/java/com/example/swcompetitionproject/dto/request/user/UserInfoDto.java
@@ -8,9 +8,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ModifyUserNameDto {
-
+public class UserInfoDto {
     @NotBlank(message = "사용 할 이름을 입력해 주세요.")
     private String name;
+    @NotBlank(message = "사용자의 성별을 입력해 주세요.")
+    private String gender;
 }
 
