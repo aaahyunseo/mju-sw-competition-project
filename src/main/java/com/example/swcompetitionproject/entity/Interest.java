eec4fa5f6848b424c585a706b4a19b8dbfd1fcfd
aaahyunseo <hs030608@naver.com>
2024-08-18 05:15:58 +0900

    feat: Interest 엔티티 추가 - #64

---
+++ src/main/java/com/example/swcompetitionproject/entity/Interest.java
@@ -0,0 +1,23 @@
+package com.example.swcompetitionproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "interests")
+public class Interest extends BaseEntity{
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
