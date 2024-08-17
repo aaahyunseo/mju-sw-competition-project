183d624a6aa78abf1c64604d1b50147be7f832c1
aaahyunseo <hs030608@naver.com>
2024-08-17 19:28:41 +0900

    feat: ChatMessageDto 수정 - #52

--- src/main/java/com/example/swcompetitionproject/dto/request/chatting/ChatMessageDto.java
+++ src/main/java/com/example/swcompetitionproject/dto/request/chatting/ChatMessageDto.java
@@ -5,10 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class ChatMessageDto {
+    @NotBlank(message = "roomId를 입력해주세요.")
+    private UUID roomId;
     @NotBlank(message = "메세지 내용을 입력해주세요.")
     private String content;
+    @NotBlank(message = "메세지 발신자를 입력해주세요.")
+    private String sender;
 }
