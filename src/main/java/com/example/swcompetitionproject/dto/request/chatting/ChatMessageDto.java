72a76c6ba087b8a1303e1034b14fc1427cea56bc
aaahyunseo <hs030608@naver.com>
2024-08-19 02:59:43 +0900

    feat: ChatMessageDto timestamp 추가 - #60

--- src/main/java/com/example/swcompetitionproject/dto/request/chatting/ChatMessageDto.java
+++ src/main/java/com/example/swcompetitionproject/dto/request/chatting/ChatMessageDto.java
@@ -1,15 +1,15 @@
 package com.example.swcompetitionproject.dto.request.chatting;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class ChatMessageDto {
     @NotBlank(message = "roomId를 입력해주세요.")
     private UUID roomId;
@@ -17,4 +17,5 @@ public class ChatMessageDto {
     private String content;
     @NotBlank(message = "메세지 발신자를 입력해주세요.")
     private String sender;
+    private LocalDateTime timestamp; // 타임스탬프 필드 추가
 }
