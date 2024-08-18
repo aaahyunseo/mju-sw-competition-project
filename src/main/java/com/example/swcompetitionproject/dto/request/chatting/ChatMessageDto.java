5adb75ea51898653c3acab74d296ac96901e8560
aaahyunseo <hs030608@naver.com>
2024-08-19 07:30:26 +0900

    feat: ChatMessageDto 수정 - #60

--- src/main/java/com/example/swcompetitionproject/dto/request/chatting/ChatMessageDto.java
+++ src/main/java/com/example/swcompetitionproject/dto/request/chatting/ChatMessageDto.java
@@ -13,9 +13,8 @@ import java.util.UUID;
 public class ChatMessageDto {
     @NotBlank(message = "roomId를 입력해주세요.")
     private UUID roomId;
-    @NotBlank(message = "메세지 내용을 입력해주세요.")
     private String content;
     @NotBlank(message = "메세지 발신자를 입력해주세요.")
     private String sender;
-    private LocalDateTime timestamp; // 타임스탬프 필드 추가
+    private LocalDateTime timestamp;
 }
