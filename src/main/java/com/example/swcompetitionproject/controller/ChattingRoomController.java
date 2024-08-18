2ad7bcf801302f7c3bd7ccbf82642c4294edc8b7
aaahyunseo <hs030608@naver.com>
2024-08-19 00:19:36 +0900

    feat: ChattingRoomController 다른 유저 채팅방 입장하기 추가 - #60

--- src/main/java/com/example/swcompetitionproject/controller/ChattingRoomController.java
+++ src/main/java/com/example/swcompetitionproject/controller/ChattingRoomController.java
@@ -1,10 +1,12 @@
 package com.example.swcompetitionproject.controller;
 
 import com.example.swcompetitionproject.authentication.AuthenticatedUser;
+import com.example.swcompetitionproject.dto.request.board.RoomIdDto;
 import com.example.swcompetitionproject.dto.response.ResponseDto;
 import com.example.swcompetitionproject.dto.response.chatting.ChattingRoomListData;
 import com.example.swcompetitionproject.entity.User;
 import com.example.swcompetitionproject.service.ChattingService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class ChattingRoomController {
     private final ChattingService chattingService;
 
-    // 채팅방 목록 조회
+    /**
+     * 채팅방 목록 조회
+     **/
     @GetMapping
     public ResponseEntity<ResponseDto<ChattingRoomListData>> getRoomList(@AuthenticatedUser User user) {
         ChattingRoomListData chattingRoomListData = chattingService.getRoomList(user);
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "채팅 목록 조회 완료", chattingRoomListData), HttpStatus.OK);
+    }
+
+    /**
+     * 다른 유저의 채팅방 추가하기
+     **/
+    @PostMapping
+    public ResponseEntity<ResponseDto<Void>> getRoomList(@AuthenticatedUser User user, @Valid @RequestBody RoomIdDto roomIdDto) {
+        chattingService.addUserToRoom(user, roomIdDto);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "채팅방 추가 완료"), HttpStatus.OK);
     }
 }
