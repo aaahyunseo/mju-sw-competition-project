e583e9de4ff7b10e4101668622ae085219dc67d5
aaahyunseo <hs030608@naver.com>
2024-08-19 00:19:01 +0900

    feat: ChatMessageController 어노테이션 추가 및 삭제 - #60

--- src/main/java/com/example/swcompetitionproject/controller/ChatMessageController.java
+++ src/main/java/com/example/swcompetitionproject/controller/ChatMessageController.java
@@ -6,6 +6,7 @@ import com.example.swcompetitionproject.entity.Message;
 import com.example.swcompetitionproject.entity.User;
 import com.example.swcompetitionproject.service.ChattingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -20,6 +21,7 @@ import java.util.UUID;
  * (WebSocket 연결, 전송, 해제)
  **/
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ChatMessageController {
@@ -31,25 +33,34 @@ public class ChatMessageController {
      **/
     @MessageMapping("/ws/chat/{roomId}/enter")
     public void enter(@DestinationVariable UUID roomId, @AuthenticatedUser User user) {
-        // 새로운 유저 판단
-        boolean isNewUser = chattingService.isNewUserInRoom(user, roomId);
 
-        // 사용자가 처음 입장하는 경우에만 입장 메시지 전송
-        if (isNewUser) {
-            // 사용자를 채팅방에 추가
-            chattingService.addUserToRoom(user, roomId);
-            // 입장 메시지 생성 및 전송
-            ChatMessageDto enterMessageDto = new ChatMessageDto(roomId, user.getName() + "님이 입장하셨습니다.", user.getStudentNumber());
-            template.convertAndSend("/sub/ws/chat/room/" + roomId, enterMessageDto);
-        }
+//        // 사용자가 처음 입장하는 경우에만 입장 메시지 전송
+//        if (chattingService.isNewUserInRoom(user, roomId)) {
+//            // 사용자를 채팅방에 추가
+//            chattingService.addUserToRoom(user, roomId);
+//            // 입장 메시지 생성 및 전송
+//            ChatMessageDto enterMessageDto = new ChatMessageDto(roomId, user.getName() + "님이 입장하셨습니다.", user.getName());
+//            log.info("Enter message DTO: {}", enterMessageDto);
+//            template.convertAndSend("/sub/ws/chat/room/" + roomId, enterMessageDto);
+//            log.info("Enter message sent.-입장 메시지 보내짐: {}",enterMessageDto.getContent());
+//        }else{
+//          // 기존 메시지들 전송 (처음 입장하는 유저가 아닌 경우에만)
+//          log.info("기존 메세지 보내기 전 : 여기까지는 오나?");
+//          List<Message> previousMessages = chattingService.getMessagesByRoomId(roomId);
+//          for (Message message : previousMessages) {
+//              ChatMessageDto messageDto = new ChatMessageDto(roomId, message.getContent(), message.getSender());
+//              template.convertAndSend("/sub/ws/chat/room/" + roomId, messageDto);
+//              log.info("Enter message sent.-기존 메시지 보내기 완료");
+//          }
+//        }
 
         // 기존 메시지들 전송 (처음 입장하는 유저가 아닌 경우에만)
-        if (!isNewUser) {
-            List<Message> previousMessages = chattingService.getMessagesByRoomId(roomId);
-            for (Message message : previousMessages) {
-                ChatMessageDto messageDto = new ChatMessageDto(roomId, message.getContent(), message.getSender());
-                template.convertAndSend("/sub/ws/chat/room/" + roomId, messageDto);
-            }
+        log.info("기존 메세지 보내기 전 : 여기까지는 오나?");
+        List<Message> previousMessages = chattingService.getMessagesByRoomId(roomId);
+        for (Message message : previousMessages) {
+            ChatMessageDto messageDto = new ChatMessageDto(roomId, message.getContent(), message.getSender());
+            template.convertAndSend("/sub/ws/chat/room/" + roomId, messageDto);
+            log.info("Enter message sent.-기존 메시지 보내기 완료");
         }
     }
 
@@ -60,8 +71,10 @@ public class ChatMessageController {
     public void message(@Payload ChatMessageDto message) {
         // MessageRepository 에 메시지 저장
         chattingService.saveMessage(message);
+        log.info("message DTO: {}", message);
         // 메시지 전송
         template.convertAndSend("/sub/ws/chat/room/" + message.getRoomId(), message);
+        log.info("message sent.:{}", message.getContent());
     }
 
     /**
@@ -73,7 +86,7 @@ public class ChatMessageController {
         chattingService.removeUserFromRoom(user, roomId);
 
         // 퇴장 메시지 생성 및 전송
-        ChatMessageDto quitMessage = new ChatMessageDto(roomId, user.getName() + "님이 퇴장하셨습니다.", user.getStudentNumber());
+        ChatMessageDto quitMessage = new ChatMessageDto(roomId, user.getName() + "님이 퇴장하셨습니다.", user.getName());
         template.convertAndSend("/sub/ws/chat/room/" + roomId, quitMessage);
     }
 }
