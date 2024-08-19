919a79d635052a66e42cd20c676794238c521c57
aaahyunseo <hs030608@naver.com>
2024-08-19 18:08:30 +0900

    feat: 예외 처리 정리 - #84

--- src/main/java/com/example/swcompetitionproject/exception/ErrorCode.java
+++ src/main/java/com/example/swcompetitionproject/exception/ErrorCode.java
@@ -13,9 +13,9 @@ public enum ErrorCode {
     INVALID_TOKEN("4010", "유효하지 않은 토큰입니다."),
     INVALID_DORMITORY("4012", "유효하지 않은 기숙사입니다."),
     INVALID_GENDER("4013", "유효하지 않은 성별입니다."),
-    ROOM_FULL("4014", "채팅방 인원이 가득찼습니다."),
-    ALREADY_LIKED("4015", "이미 좋아요를 눌렀습니다."),
-    USER_ALREADY_IN_ROOM("4016", "이미 채팅방에 추가된 멤버입니다."),
+    ROOM_FULL("4014", "채팅방에 참여 가능한 인원이 가득찼습니다."),
+    ALREADY_LIKED("4015", "이미 관심글로 등록되었습니다."),
+    USER_ALREADY_IN_ROOM("4016", "이미 참여중인 채팅방입니다."),
 
     //ForbiddenException
     NO_ACCESS("4030", "접근 권한이 없습니다."),
@@ -27,7 +27,7 @@ public enum ErrorCode {
     ROOM_NOT_FOUND("4042", "방을 찾을 수 없습니다."),
     BOARD_NOT_FOUND("4043", "게시글을 찾을 수 없습니다."),
     CATEGORY_NOT_FOUND("4044", "카테고리를 찾을 수 없습니다"),
-    LIKE_NOT_FOUND("4045", "좋아요를 찾을 수 없습니다."),
+    LIKE_NOT_FOUND("4045", "관심글을 찾을 수 없습니다."),
 
     //ConflictException
     DUPLICATED_NAME("4090", "이미 사용중인 이름입니다."),
