e4d61fd8d1a38bd6b8c817d348239a16dfb75019
aaahyunseo <hs030608@naver.com>
2024-08-19 18:53:31 +0900

    feat: BadRequestException 예외코드만 처리하는 메서드 추가 - #88

--- src/main/java/com/example/swcompetitionproject/exception/BadRequestException.java
+++ src/main/java/com/example/swcompetitionproject/exception/BadRequestException.java
@@ -1,6 +1,9 @@
 package com.example.swcompetitionproject.exception;
 
 public class BadRequestException extends CustomException {
+    public BadRequestException(ErrorCode errorCode) {
+        super(errorCode);
+    }
     public BadRequestException(ErrorCode errorCode, String detail) {
         super(errorCode, detail);
     }
